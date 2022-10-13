b3149e10b15e5047afacddf0272a856621f857c5
Nick Spinale <nick@nickspinale.com>
2022-10-13 08:26:00 +0200

    Show line numbers

--- sanskan.py
+++ sanskan.py
@@ -2,6 +2,7 @@ import sys
 import re
 import glob
 import json
+import bisect
 from pathlib import Path
 from argparse import ArgumentParser, FileType
 
@@ -22,7 +23,7 @@ def main():
         print('Error deserializing query from {}: {}'.format(args.query_file.name, e), file=sys.stderr)
         sys.exit(1)
 
-    print('query: {}'.format(query))
+    print('[query] {}'.format(query))
 
     query.run()
 
@@ -49,19 +50,18 @@ class Query:
         self.fragments = obj['fragments']
 
         flags = re.IGNORECASE
-        self.fragment_regexes = [ re.compile(re.escape(fragment), flags) for fragment in self.fragments ]
+        self.fragment_regexes = { fragment: re.compile(re.escape(fragment), flags) for fragment in self.fragments }
 
     def __str__(self):
-        conjunction = ' & '.join(map(repr, self.fragments))
+        conjunction = ' | '.join(map(repr, self.fragments))
         scope = ', '.join(map(str, self.directories))
         return "[ {} ] in [ {} ]".format(conjunction, scope)
 
     def search_text(self, text):
-        for regex in self.fragment_regexes:
-            match = regex.search(text)
-            if match is None:
-                return False
-        return True
+        newline_index = NewlineIndex(text)
+        for fragment, regex in self.fragment_regexes.items():
+            for match in regex.finditer(text):
+                yield fragment, newline_index.line_index(match.start())
 
     def run(self):
         num_matches = 0
@@ -74,11 +74,24 @@ class Query:
             for text_path in text_paths:
                 with open(text_path, encoding='utf-8') as f:
                     text = f.read()
-                if self.search_text(text):
-                    print('match: {}'.format(text_path))
+                for fragment_regex, line_index in self.search_text(text):
+                    line_number = line_index + 1
+                    print('[match] {} at {}:{}'.format(fragment_regex, text_path, line_number))
                     num_matches += 1
 
-        print('summary: {} matches'.format(num_matches))
-    
+        print('[summary] {} matches'.format(num_matches))
+
+class NewlineIndex:
+
+    def __init__(self, text):
+        lf_indices = []
+        r = re.compile('\n')
+        for match in r.finditer(text):
+            lf_indices.append(match.start())
+        self.lf_indices = lf_indices
+
+    def line_index(self, char_index):
+        return bisect.bisect_left(self.lf_indices, char_index)
+
 if __name__ == '__main__':
     main()
